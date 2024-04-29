f1d828b5fcd81815a17d412b1eed0ee18ebae7d7
Mehdi Chikhi <ci_mi@outlook.fr>
2024-04-29 14:19:31 +0100

    finished lecture 83

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -36,7 +36,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       selectedAnswers = [];
       setState(() {
-        activeScreen = const ResultsScreen();
+        activeScreen = ResultsScreen(chosenAnswers: selectedAnswers);
       });
     }
   }
